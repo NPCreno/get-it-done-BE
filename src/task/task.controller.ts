f51e3f7fde3009e5b9e1d45bf380d5ac075ba394
npcreno <laurence.divinagracia25@gmail.com>
2025-06-03 07:48:32 +0000

    get task by proj with start and end date params

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, UseGuards} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { TaskInstance } from './models/taskInstance.entity';
@@ -6,28 +14,32 @@ import { AuthorizeGuard } from 'src/auth/guards/authorize.guard';
 import { TaskTemplate } from './models/taskTemplate.entity';
 @Controller('api/task')
 export class TaskController {
-    constructor(private taskService: TaskService){}
+  constructor(private taskService: TaskService) {}
 
-    @UseGuards(AuthorizeGuard)
-    @Post('create')
-    async create(@Body() dto: CreateTaskDto): Promise<{
+  @UseGuards(AuthorizeGuard)
+  @Post('create')
+  async create(@Body() dto: CreateTaskDto): Promise<{
     status: string;
     message: string;
     data?: TaskInstance | TaskTemplate;
     error?: any;
-    }> {
+  }> {
     return this.taskService.createTask(dto);
-    }
+  }
 
-    @UseGuards(AuthorizeGuard)
-    @Get('getAll/:user_id')
-    findAllForUser(@Param('user_id') user_id: string) {
+  @UseGuards(AuthorizeGuard)
+  @Get('getTasksByUser/:user_id')
+  findAllForUser(@Param('user_id') user_id: string) {
     return this.taskService.findAllTasksForUser(user_id);
-    }
+  }
 
-    @UseGuards(AuthorizeGuard)
-    @Get('getAllfromProj/:project_id')
-    findAllTasksForProject(@Param('project_id') project_id: string) {
-    return this.taskService.findAllTasksForProject(project_id);
-    }
+  @UseGuards(AuthorizeGuard)
+  @Get('getTasksByProj/:project_id')
+  getTasksByProj(
+    @Param('project_id') project_id: string,
+    @Query('startDate') startDate?: string,
+    @Query('endDate') endDate?: string,
+  ) {
+    return this.taskService.getTasksByProj(project_id, startDate, endDate);
+  }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,6 +1,10 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Between, IsNull, Repository } from 'typeorm';
 import { TaskInstance } from './models/taskInstance.entity';
 import { TaskTemplate } from './models/taskTemplate.entity';
 import { CreateTaskDto } from './dto/create-task-dto';
@@ -10,166 +14,215 @@ import { Projects } from 'src/projects/models/projects.entity';
 import { TaskGeneratorService } from './taskGenerator.service';
 import { OnModuleInit } from '@nestjs/common';
 @Injectable()
-export class TaskService implements OnModuleInit{
-    constructor (
-        @InjectRepository(TaskTemplate) private readonly taskTemplateRepository: Repository<TaskTemplate>,
-        @InjectRepository(TaskInstance) private readonly taskInstanceRepository: Repository<TaskInstance>,
-        @InjectRepository(Users) private readonly usersRepository: Repository<Users>,
-        @InjectRepository(Projects) private readonly projectsRepository: Repository<Projects>,
-        private readonly taskGeneratorService: TaskGeneratorService,
-    ){}
-    async onModuleInit() {
-      await this.taskGeneratorService.generateInstancesForCurrentMonth();  //trigger cronjob for generating task instances from task template
-    }
+export class TaskService implements OnModuleInit {
+  constructor(
+    @InjectRepository(TaskTemplate)
+    private readonly taskTemplateRepository: Repository<TaskTemplate>,
+    @InjectRepository(TaskInstance)
+    private readonly taskInstanceRepository: Repository<TaskInstance>,
+    @InjectRepository(Users)
+    private readonly usersRepository: Repository<Users>,
+    @InjectRepository(Projects)
+    private readonly projectsRepository: Repository<Projects>,
+    private readonly taskGeneratorService: TaskGeneratorService,
+  ) {}
+  async onModuleInit() {
+    await this.taskGeneratorService.generateInstancesForCurrentMonth(); //trigger cronjob for generating task instances from task template
+  }
 
-    private generateTaskInstanceId(): string {
-      const randomNumber = Math.floor(Math.random() * 1_000_000_000); // 0 to 999,999,999
-      return 'task-' + randomNumber.toString().padStart(9, '0');
-    }
+  private generateTaskInstanceId(): string {
+    const randomNumber = Math.floor(Math.random() * 1_000_000_000); // 0 to 999,999,999
+    return 'task-' + randomNumber.toString().padStart(9, '0');
+  }
 
-    private generateTaskTemplateId(): string {
-      const randomNumber = Math.floor(Math.random() * 1_000_000_000); // 0 to 999,999,999
-      return 'taskTemplate-' + randomNumber.toString().padStart(9, '0');
-    }
+  private generateTaskTemplateId(): string {
+    const randomNumber = Math.floor(Math.random() * 1_000_000_000); // 0 to 999,999,999
+    return 'taskTemplate-' + randomNumber.toString().padStart(9, '0');
+  }
 
-    async createTask(taskDto: CreateTaskDto): Promise<{ status: string; message: string; data?: TaskInstance | TaskTemplate; error?: any }> {
-      try {
-        // find user_id
-        const user_id = taskDto.user_id
-        const userExist = await this.usersRepository.findOne({ where: { user_id } }); 
-        if(!userExist){
-          throw new NotFoundException(`User with ID ${user_id} not found`);
+  async createTask(
+    taskDto: CreateTaskDto,
+  ): Promise<{
+    status: string;
+    message: string;
+    data?: TaskInstance | TaskTemplate;
+    error?: any;
+  }> {
+    try {
+      // find user_id
+      const user_id = taskDto.user_id;
+      const userExist = await this.usersRepository.findOne({
+        where: { user_id },
+      });
+      if (!userExist) {
+        throw new NotFoundException(`User with ID ${user_id} not found`);
+      }
+
+      // find project_id
+      const project_id = taskDto?.project_id;
+      const projectExists = await this.projectsRepository.findOne({
+        where: { project_id },
+      });
+      if (!projectExists) {
+        throw new NotFoundException(`Project with ID ${project_id} not found`);
+      }
+
+      let task_id: string;
+      let taskTemplate_id: string;
+      let exists = true;
+      if (taskDto.isRecurring) {
+        if (!taskDto.repeat_every)
+          throw new BadRequestException(
+            'repeat_every is required for recurring tasks',
+          );
+        if (!taskDto.start_date)
+          throw new BadRequestException(
+            'start_date is required for recurring tasks',
+          );
+        if (
+          taskDto.repeat_every === 'Week' &&
+          (!taskDto.repeat_days || taskDto.repeat_days.length === 0)
+        ) {
+          throw new BadRequestException(
+            'repeat days is required when repeat every is "Week"',
+          );
         }
-
-        // find project_id
-        const project_id = taskDto?.project_id
-        const projectExists = await this.projectsRepository.findOne({ where: { project_id } }); 
-        if(!projectExists){
-          throw new NotFoundException(`Project with ID ${project_id} not found`);
-        }
-
-        let task_id: string;
-        let taskTemplate_id: string;
-        let exists = true;
-        if(taskDto.isRecurring){
-          if (!taskDto.repeat_every) throw new BadRequestException('repeat_every is required for recurring tasks');
-          if (!taskDto.start_date) throw new BadRequestException('start_date is required for recurring tasks');
-          if (taskDto.repeat_every === 'Week' && (!taskDto.repeat_days || taskDto.repeat_days.length === 0)) {
-            throw new BadRequestException('repeat days is required when repeat every is "Week"');
-          }
-            do {
-            taskTemplate_id = this.generateTaskTemplateId(); 
-            const existing = await this.taskTemplateRepository.findOne({ where: { taskTemplate_id } }); 
-            exists = !!existing;
-          } while (exists);
-          const {
-            user_id,
-            title,
-            description,
-            repeat_every,
-            repeat_days,
-            start_date,
-            end_date,
-            project_id,
-          } = taskDto;
+        do {
+          taskTemplate_id = this.generateTaskTemplateId();
+          const existing = await this.taskTemplateRepository.findOne({
+            where: { taskTemplate_id },
+          });
+          exists = !!existing;
+        } while (exists);
+        const {
+          user_id,
+          title,
+          description,
+          repeat_every,
+          repeat_days,
+          start_date,
+          end_date,
+          project_id,
+        } = taskDto;
 
         const taskTemplate = this.taskTemplateRepository.create({
-        taskTemplate_id,
-        user_id,
-        title,
-        description,
-        repeat_every,
-        repeat_days,
-        start_date,
-        end_date,
-        project_id
+          taskTemplate_id,
+          user_id,
+          title,
+          description,
+          repeat_every,
+          repeat_days,
+          start_date,
+          end_date,
+          project_id,
         } as DeepPartial<TaskTemplate>);
 
-        const savedTaskTemplate = await this.taskTemplateRepository.save(taskTemplate);
-        await this.taskGeneratorService.generateInstancesForCurrentMonth();  //trigger cronjob for generating task instances from task template
+        const savedTaskTemplate =
+          await this.taskTemplateRepository.save(taskTemplate);
+        await this.taskGeneratorService.generateInstancesForCurrentMonth(); //trigger cronjob for generating task instances from task template
         return {
-          status: "success",
-          message: "Recurring task created successfully",
+          status: 'success',
+          message: 'Recurring task created successfully',
           data: savedTaskTemplate,
         };
-        }else{
-          do {
-            // Generate unique task_id
-            task_id = this.generateTaskInstanceId(); 
-            const existing = await this.taskInstanceRepository.findOne({ where: { task_id } }); 
-            exists = !!existing;
-          } while (exists);
+      } else {
+        do {
+          // Generate unique task_id
+          task_id = this.generateTaskInstanceId();
+          const existing = await this.taskInstanceRepository.findOne({
+            where: { task_id },
+          });
+          exists = !!existing;
+        } while (exists);
 
-          // Create and save the new project
-          let taskInstance: TaskInstance;
-          if (taskDto.project_id) {
-            taskInstance = this.taskInstanceRepository.create({
-              ...taskDto,
-              priority: taskDto.priority as 'Low' | 'Medium' | 'High',
-              status: taskDto.status as 'Pending' | 'Complete',
-              project: { project_id: taskDto.project_id },  // link project by project_id
-              user: { user_id: taskDto.user_id },  // link user by user_id
-              task_id,
-            });
-          }
-          else{
-            taskInstance = this.taskInstanceRepository.create({
+        // Create and save the new project
+        let taskInstance: TaskInstance;
+        if (taskDto.project_id) {
+          taskInstance = this.taskInstanceRepository.create({
             ...taskDto,
             priority: taskDto.priority as 'Low' | 'Medium' | 'High',
             status: taskDto.status as 'Pending' | 'Complete',
-            user: { user_id: taskDto.user_id },  // link user by user_id
+            project: { project_id: taskDto.project_id }, // link project by project_id
+            user: { user_id: taskDto.user_id }, // link user by user_id
             task_id,
-            });
-          }
-
-          const savedTask = await this.taskInstanceRepository.save(taskInstance);
-
-          return {
-          status: "success",
-          message: "Task created successfully",
-          data: savedTask,
-          };
+          });
+        } else {
+          taskInstance = this.taskInstanceRepository.create({
+            ...taskDto,
+            priority: taskDto.priority as 'Low' | 'Medium' | 'High',
+            status: taskDto.status as 'Pending' | 'Complete',
+            user: { user_id: taskDto.user_id }, // link user by user_id
+            task_id,
+          });
         }
-      } catch (error) {
-          console.error("Error creating task:", error);
-          return {
-          status: "error",
-          message: "Failed to create task",
-          error: error?.message || error,
-          };
-      }
-    }
 
-    async findAllTasksForUser(user_id: string): Promise<any[]> { // change return type to any since user is not returned[] 
-      const data = await this.taskInstanceRepository.find({
-          where: { user: { user_id } },
-          relations: ['user'],
-          withDeleted: false,
+        const savedTask = await this.taskInstanceRepository.save(taskInstance);
+
+        return {
+          status: 'success',
+          message: 'Task created successfully',
+          data: savedTask,
+        };
+      }
+    } catch (error) {
+      console.error('Error creating task:', error);
+      return {
+        status: 'error',
+        message: 'Failed to create task',
+        error: error?.message || error,
+      };
+    }
+  }
+
+  async findAllTasksForUser(user_id: string): Promise<any[]> {
+    // change return type to any since user is not returned[]
+    const data = await this.taskInstanceRepository.find({
+      where: { user: { user_id } },
+      relations: ['user'],
+      withDeleted: false,
+    });
+    if (data.length === 0) {
+      throw new NotFoundException(`No tasks found for user ID ${user_id}`);
+    }
+    const sanitizedData = data.map(({ project, ...rest }) => ({
+      ...rest,
+      project_id: project.project_id, // only project_id, not full project
+    }));
+    return sanitizedData;
+  }
+
+  async getTasksByProj(
+    project_id: string,
+    startDate?: string,
+    endDate?: string,
+  ): Promise<any[]> {
+    // change return type to any since user is not returned[]
+
+    const where: any[] = [{ project: { project_id }, due_date: IsNull() }];
+
+    if (startDate && endDate) {
+      where.push({
+        project: { project_id },
+        due_date: Between(new Date(startDate), new Date(endDate)),
       });
-      if (data.length === 0) {
-          throw new NotFoundException(`No tasks found for user ID ${user_id}`);
-      }
-      const sanitizedData = data.map(({ project, ...rest }) => ({
-        ...rest,
-        project_id: project.project_id,  // only project_id, not full project
-      }));
-      return sanitizedData;
     }
 
-    async findAllTasksForProject(project_id: string): Promise<any[]> { // change return type to any since user is not returned[] 
-      const data = await this.taskInstanceRepository.find({
-          where: { project: { project_id } },
-          relations: ['project'],
-          withDeleted: false,
-      });
-      if (data.length === 0) {
-          throw new NotFoundException(`No tasks found for project ID ${project_id}`);
-      }
-      const sanitizedData = data.map(({ project, ...rest }) => ({
-        ...rest,
-        project_id: project.project_id,  // only project_id, not full project
-      }));
+    const data = await this.taskInstanceRepository.find({
+      where,
+      relations: ['project'],
+      withDeleted: false,
+    });
 
-      return sanitizedData;
+    if (data.length === 0) {
+      throw new NotFoundException(
+        `No tasks found for project ID ${project_id}`,
+      );
     }
+    const sanitizedData = data.map(({ project, ...rest }) => ({
+      ...rest,
+      project_id: project.project_id, // only project_id, not full project
+    }));
+
+    return sanitizedData;
+  }
 }
