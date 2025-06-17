52de7b79938ebf73070fde9fa69caf4dbd80a950
NPCreno <laurence.divinagracia25@gmail.com>
2025-06-17 20:53:12 +0800

    enforced ownership in projects controller

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -6,6 +6,8 @@ import {
     Param,
     Patch,
     Post,
+    Req,
+    UnauthorizedException,
     UseGuards,
   } from '@nestjs/common';
   import { ProjectsService } from './projects.service';
@@ -37,18 +39,26 @@ import { SanitizedProject } from './interfaces/sanitizedProject';
 
     @UseGuards(AuthorizeGuard)
     @Get(':project_id')
-    findOne(@Param('project_id') project_id: string): Promise<Projects> {
+    findOne(@Param('project_id') project_id: string, @Req() req: Request): Promise<Projects> {
+      const tokenUserId = req['user'];
+      if (tokenUserId.user.user_id !== project_id) {
+          throw new UnauthorizedException('Access denied: Not your data.');
+      }
       return this.projectsService.findOne(project_id);
     }
   
     @UseGuards(AuthorizeGuard)
     @Post('create')
-    async create(@Body() dto: CreateProjectDto): Promise<{
+    async create(@Body() dto: CreateProjectDto, @Req() req: Request): Promise<{
       status: string;
       message: string;
       data?: Projects;
       error?: any;
     }> {
+      const tokenUserId = req['user'];
+      if (tokenUserId.user.user_id !== dto.user_id) {
+          throw new UnauthorizedException('Access denied: Not your data.');
+      }
       return this.projectsService.createProject(dto);
     }
   
@@ -57,24 +67,37 @@ import { SanitizedProject } from './interfaces/sanitizedProject';
     async update(
       @Param('project_id') project_id: string,
       @Body() updateProjectDto: UpdateProjectDto,
+      @Req() req: Request,
     ): Promise<Projects> {
+      const tokenUserId = req['user'];
+      if (tokenUserId.user.user_id !== project_id) {
+          throw new UnauthorizedException('Access denied: Not your data.');
+      }
       return this.projectsService.updateOne(project_id, updateProjectDto);
     }
   
     @UseGuards(AuthorizeGuard)
     @Delete(':project_id')
-    async softDeleteOne(@Param('id') project_id: string): Promise<Projects> {
+    async softDeleteOne(@Param('id') project_id: string, @Req() req: Request): Promise<Projects> {
+      const tokenUserId = req['user'];
+      if (tokenUserId.user.user_id !== project_id) {
+          throw new UnauthorizedException('Access denied: Not your data.');
+      }
       return this.projectsService.softDeleteOne(project_id);
     }
   
     @UseGuards(AuthorizeGuard)
     @Delete(':project_id/hard')
-    async hardDeleteOne(@Param('id') project_id: string): Promise<{
+    async hardDeleteOne(@Param('id') project_id: string, @Req() req: Request): Promise<{
         status: string;
         message: string;
         data?: Projects;
         error?: any;
       }> {
+      const tokenUserId = req['user'];
+      if (tokenUserId.user.user_id !== project_id) {
+          throw new UnauthorizedException('Access denied: Not your data.');
+      }
     return this.projectsService.hardDeleteOne(project_id);
     }
   }
